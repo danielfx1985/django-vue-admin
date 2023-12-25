29ed786905183771a557ceb82a0ea022988a8e7c
Daniel <oncwnuEUDBll3ts6drwofMSs6szc@git.weixin.qq.com>
2023-12-25 15:58:17 +0800

    t9

--- backend/docker_start.sh
+++ backend/docker_start.sh
@@ -3,5 +3,5 @@
  python manage.py migrate
  python manage.py init -y
 #gunicorn -c gunicorn_conf.py application.asgi:application
-#uvicorn application.asgi:application --port 80 --host 0.0.0.0 --workers 4
-python manage.py runserver 0.0.0.0:80
+uvicorn application.asgi:application --port 80 --host 0.0.0.0 --workers 4
+#python manage.py runserver 0.0.0.0:80
